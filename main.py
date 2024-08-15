177db53273b19e580a185876a210518952f6e2f4
Elleuch Bilel <bilel.elleuch96@gmail.com>
2024-08-15 18:37:35 +0200

    Initial Flask app with PostgreSQL and MongoDB

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import psycopg2
+from pymongo import MongoClient
+from flask import Flask
+
+app = Flask(__name__)
+
+uri = "mongodb+srv://bilelelleuch96:<password>@cluster0.qdq3p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+pg_conn = psycopg2.connect(
+    host=os.environ.get('POSTGRES_HOST'),
+    database=os.environ.get('POSTGRES_DB'),
+    user=os.environ.get('POSTGRES_USER'),
+    password=os.environ.get('POSTGRES_PASS')
+)
+
+mongo_client = MongoClient(os.environ.get('MONGO_URI'))
+mongo_db = mongo_client['your_mongo_database']
+
+@app.route('/')
+def home():
+  return 'Hello from Flask!'
+
+@app.route('/test-postgres')
+def test_postgres():
+    try:
+        cur = pg_conn.cursor()
+        cur.execute("SELECT version();")
+        db_version = cur.fetchone()
+        return f"Connected to PostgreSQL: {db_version[0]}"
+    except Exception as e:
+        return f"Failed to connect to PostgreSQL: {str(e)}"
+    finally:
+        cur.close()
+
+@app.route('/test-mongo')
+def test_mongo():
+    try:
+        db_list = mongo_client.list_database_names()
+        return f"Connected to MongoDB: Databases - {db_list}"
+    except Exception as e:
+        return f"Failed to connect to MongoDB: {str(e)}"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
---
+++ vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "HIii"
+export POSTGRES_HOST='c7u1tn6bvvsodf.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com'
+export POSTGRES_DB='d253807rvm2qmq'
+export POSTGRES_USER='uru2gvnk704tn'
+export POSTGRES_PASS='p7e9c187ae6de596bca1d67608a9685ee3316332c4913ca919b1c27ae72214cae'
+export MONGO_URI='your_mongodb_uri'
+export MONGO_DB='your_mongodb_db'
+export MONGO_USER='your_mongodb_user'
+export MONGO_PASS='your_mongodb_password'
+
+echo "End"
