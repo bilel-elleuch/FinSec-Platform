f59ab744662e7ad758ba4b78b8cb1faf975265bc
Elleuch Bilel <bilel.elleuch96@gmail.com>
2024-08-15 21:33:54 +0200

    kchaw

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def ip():
 @app.route('/info')
 def toto():
     print("ya3tik 3asba ya kchaw")
-
+    return None
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
